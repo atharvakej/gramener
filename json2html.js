fecd7c9d7b86f3d5169c9fd5fa0eea5ed233ff94
Atharva Kumar <atharvados12121@gmail.com>
2024-11-06 14:58:44 +0530

    Update json2html.js

--- json2html.js
+++ json2html.js
@@ -3,12 +3,12 @@ export default function json2html(data) {
     const headers = Array.from(new Set(data.flatMap(obj => Object.keys(obj))));
 
     // Start building the HTML table as a string
-    let html = <table data-user="atharvados12121@gmail.com">;
+    let html = `<table data-user="atharvados12121@gmail.com">`;
     
     // Add the table headers
     html += "<thead><tr>";
     headers.forEach(header => {
-        html += <th>${header}</th>;
+        html += `<th>${header}</th>`;
     });
     html += "</tr></thead>";
     
@@ -17,7 +17,7 @@ export default function json2html(data) {
     data.forEach(row => {
         html += "<tr>";
         headers.forEach(header => {
-            html += <td>${row[header] !== undefined ? row[header] : ''}</td>;
+            html += `<td>${row[header] !== undefined ? row[header] : ''}</td>`;
         });
         html += "</tr>";
     });
